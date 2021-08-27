4f464c27c720797512f50b991945872ce46090d7
sindharta <sindharta@gmail.com>
2021-08-27 16:11:22 +0900

    Add DictionaryExtensions
    (cherry picked from commit f28d7e4525af389f2cfd71f573c71a40c1672a7f)

---
+++ Runtime/Scripts/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Unity.MeshSync  {
+[Serializable]
+internal static class DictionaryExtensions {
+
+    internal static void ToKeyAndValues<K, V>(this Dictionary<K, V> dic, ref K[] keys, ref V[] values) {
+        int count = dic.Count;
+        keys   = new K[count];
+        values = new V[count];        
+        dic.Keys.CopyTo(keys,0);
+        dic.Values.CopyTo(values,0);
+    }
+    
+}
+
+} //end namespace
