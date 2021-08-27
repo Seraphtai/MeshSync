7f467bf1e833dfea08348f1fe296dfac1fe06aa7
sindharta <sindharta@gmail.com>
2021-08-27 15:54:18 +0900

    First implementation of FlaggedDictionary

---
+++ Runtime/Scripts/FlaggedDictionary.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Unity.MeshSync {
+
+internal class FlaggedDictionary<K,V> {
+
+    internal bool IsDirty() { return m_isDirty;}
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    
+    private Dictionary<K, V> m_dictionary;
+    private bool             m_isDirty;
+}    
+    
+} //end namespace
