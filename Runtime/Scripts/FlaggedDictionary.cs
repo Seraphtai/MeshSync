719dfc89f4896131bec07239fa1ffc8c696ea7b6
sindharta <sindharta@gmail.com>
2021-08-27 16:04:32 +0900

    add clear to flagged dictionary

--- Runtime/Scripts/FlaggedDictionary.cs
+++ Runtime/Scripts/FlaggedDictionary.cs
@@ -5,10 +5,27 @@ namespace Unity.MeshSync {
 internal class FlaggedDictionary<K,V> {
 
     internal bool IsDirty() { return m_isDirty;}
+
+    internal void SetDirty(bool dirty) { return m_isDirty;}
+    
+    internal void Clear() {
+        m_dictionary.Clear();
+        m_isDirty = true;
+    }
+    
+    void Flush(ref K[] keys, ref V[] values) {
+        if (!m_isDirty)
+            return;
+        
+        keys      = m_dictionary.Keys.ToArray();
+        values    = m_dictionary.Values.ToArray();
+        m_isDirty = false;
+    }
+    
     
 //----------------------------------------------------------------------------------------------------------------------    
     
-    private Dictionary<K, V> m_dictionary;
+    private Dictionary<K, V> m_dictionary = new Dictionary<K, V>();
     private bool             m_isDirty;
 }    
     
